4d2c312b08e898545650b2be1d5b966474307168
taylourdaniels <taylour.daniels@outlook.com>
2021-09-23 07:38:30 -0500

    Created script.js.

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1a.
+let warmHugs = "Hi, my name is Olaf and I like warm hugs.";
+
+// 1b.
+console.log(warmHugs.toUpperCase());
+
+// 1c.
+console.log(warmHugs.replace("like", "love"));
+
+// 2a.
+ let beenImpaled = "Oh, look at that. I've been impaled.";
+
+ // 2b.
+console.log(beenImpaled.slice(18,36));
+
+// 3a.
+let dotDotDot = "...";
+
+// 3b.
+let skullBones = `I don't have a skull ${dotDotDot} or bones.`;
+console.log(skullBones);
+
+// 4.
+console.log(Math.PI);
+
+// 5.
+let randomNumber = Math.floor(Math.random()*3)+1
+console.log(randomNumber)
